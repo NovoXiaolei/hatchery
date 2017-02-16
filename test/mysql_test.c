b6f11d1022d250c1d0fa4ba578fc59c69d7dda41
xiaolei <childmercy@163.com>
2017-02-15 23:31:50 -0500

    rm test/mysql_utils.h

--- test/mysql_test.c
+++ test/mysql_test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "mysql_utils.h"
+#include "../utils/mysql_utils.h"
 
 #define BUF_SIZE 1024
 
@@ -11,9 +11,14 @@ int main(int argc, char **argv){
     } 
 
     char *buf = (char*)malloc(BUF_SIZE);
-    SQL_ROW *rows = db_mysql_fmt(&mysql, buf, BUF_SIZE, "%s %s %s %s", "insert into", "user", "(name, password, uid)","values ('test', '111111', '1001'); "); 
-    if(rows){
+    SQL_ROW *rows = NULL;
+    rows = db_mysql_select(&mysql, buf, BUF_SIZE, "%s", "select * from user;"); 
+    if(rows != NULL){
         db_print_rows(rows);
+    }
+    else
+    {
+        printf("Failed to op to database : Error: %s \n", mysql_error(&mysql));
     }
     db_free_rows(rows);
 
