a48334436f5afab820cd64b0e99936d1328fcd48
hatchery <xiaolei@localhost.localdomain>
2017-07-20 16:18:29 +0800

    fix cmake files

---
+++ utils/utils.c
@@ -0,0 +1,4 @@
+#include "utils.h"
+char *getpwd(char *buf, size_t size){
+    return NULL;
+}
---
+++ utils/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <unistd.h>
+
+char *getpwd(char *buf, size_t size);
+
+#endif
